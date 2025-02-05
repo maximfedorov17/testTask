9eef72fc11e2f78f0bd57d0804ef246c293d60d3
Fedorov Maksim <elbanforever@gmail.com>
2025-02-05 23:25:53 +0500

    navigation rename

--- src/widgets/navigation/navigation.tsx
+++ src/widgets/navigation/navigation.tsx
@@ -5,13 +5,13 @@ export const Navigation = () => {
 		<>
 			<div className='tab-headers'>
 				<Link to='/' className='tab-header' data-target='content1'>
-					Вкладка 1
+					Users
 				</Link>
 				<Link to='/goods' className='tab-header' data-target='content2'>
-					Вкладка 2
+					Goods
 				</Link>
 				<Link to='/all' className='tab-header' data-target='content3'>
-					Вкладка 3
+					All
 				</Link>
 			</div>
 		</>
